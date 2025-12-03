eb72ddd916d469666ae479899d3bf028f4015567
AbyssRei <AbyssRei@outlook.com>
2025-12-04 06:57:24 +0800

    feat: 更新构建脚本，添加uv运行支持并移除requirements.txt

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-pyinstaller --name "nightreign-overlay-helper" --windowed --icon="assets\icon.ico" src\app.py
+uv run pyinstaller --name "nightreign-overlay-helper" --windowed --icon="assets\icon.ico" src\app.py
 xcopy /E /I /Y "assets" "dist\nightreign-overlay-helper\assets"
 xcopy /E /I /Y "data" "dist\nightreign-overlay-helper\data"
 copy "manual.txt" "dist\nightreign-overlay-helper\manual.txt"
